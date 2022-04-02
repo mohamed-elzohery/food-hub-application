f287845158c967f1f1e721ba8b277fce0dfb2037
mohamed-elzohery <m.zohery1998@gmail.com>
2022-04-02 16:40:55 +0200

    resolve conflicts

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,12 +1,11 @@
 import React, { useContext } from "react";
+import Header from './layout/header/Header';
+import SideBar from './UI/sidebar/SideBar';
 // import SignUpForm from "./components/SignUpForm";
 // import LoginForm from "./components/LoginForm";
 import { Navigate, Route, Routes } from "react-router-dom";
-import AuthContext from "./storeTokens/Auth-Context";
 import Error from "./components/Error";
 import Home from "./pages/Home";
-import { Navigate, Route, Routes } from "react-router-dom";
-import Error from "./components/Error";
 //uncomment if you want to change user pw
 // import Profile from "./components/Profile";
 import NavBar from "./UI/navbar/NavBar";
@@ -14,13 +13,12 @@ import Footer from "./layout/footer/Footer";
 import Cart from "./components/cart/Cart";
 import { useSelector } from "react-redux";
 function App() {
-  const authCtx = useContext(AuthContext);
-  const isLoggedIn = authCtx.isLoggedIn;
   const isSidebarShown = useSelector(state => state.UI.isSidebarShown);
   const isCartOpen = useSelector(state => state.UI.isCartOpen);
   console.log(isCartOpen)
   return (
     <div className="App">
+      <NavBar />
       <Header />
       {isSidebarShown && <SideBar />}
       {isCartOpen && <Cart />}
